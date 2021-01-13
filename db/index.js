e0caf2d323e7114425c1af048ad4a00f81dafed8
陈童 <1421911991@qq.com>
2021-01-13 11:43:28 +0800

    检测注册信息

---
+++ db/index.js
@@ -0,0 +1,18 @@
+// 导入 mysql 模块
+const mysql = require('mysql')
+
+// 创建数据库连接对象
+const db = mysql.createPool({
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'root',
+  database: 'my_db_01'
+})
+
+// db.query('select 1', (err, result) => {
+//   console.log(err)
+//   console.log(result)
+// })
+
+// 向外共享 db 数据库连接对象
+module.exports = db
--- router_handler/use.js
+++ router_handler/use.js
@@ -1,5 +1,33 @@
+// const { send } = require('process')
+const db = require('../db')
+
 exports.regUser = (req, res) => {
-  res.send('regUser ok')
+  const userInfo = req.body
+  // 1. 检测是否为空
+  if (!userInfo.username || !userInfo.password) {
+    return res.send({
+      status: 1,
+      message: '用户名或者密码不能为空!'
+    })
+  }
+
+  // 2.检测用户名是否被占用
+  const sql = `select * from ev_user where username=?`
+  db.query(sql, [userInfo.username], (err, result) => {
+    if (err) {
+      return res.send({
+        status: 1,
+        message: err.message
+      })
+    }
+    if (result.length > 0) {
+      return res.send({
+        status: 1,
+        message: '用户名被占用请更换其他的用户名'
+      })
+    }
+    res.send('可以注册')
+  })
 }
 
 exports.login = (req, res) => {
