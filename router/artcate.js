0c2fe39f00e35da6901ca40ab3da5ae448014473
陈童 <1421911991@qq.com>
2021-01-16 16:06:15 +0800

    根据ID获取文章分类

--- router/artcate.js
+++ router/artcate.js
@@ -6,7 +6,7 @@ const expressJoi = require('@escook/express-joi')
 const artcate_handler = require('../router_handler/artcate')
 
 // 导入文章分类的验证模块
-const { add_cate_schema, delete_cate_schema } = require('../schema/artcate')
+const { add_cate_schema, delete_cate_schema, get_cate_schema } = require('../schema/artcate')
 
 // 获取文章分类的列表数据
 router.get('/cates', artcate_handler.getArticleCates)
@@ -14,7 +14,8 @@ router.get('/cates', artcate_handler.getArticleCates)
 router.post('/addcates', expressJoi(add_cate_schema), artcate_handler.addArticleCates)
 // 删除文章分类的路由
 router.get('/deletecate/:id', expressJoi(delete_cate_schema), artcate_handler.deleteCateById)
-
+// 根据 Id 获取文章分类
+router.get('/cates/:id', expressJoi(get_cate_schema), artcate_handler.getArtCateById)
 
 // 向外共享路由对象
 module.exports = router
